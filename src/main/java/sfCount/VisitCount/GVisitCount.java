7940a508a0be4f0f64f8e7a6000ed1f1aebde0c0
Ming Gong <camark@live.cn>
2018-06-20 22:35:35 +0800

    防止JDBC泄露

--- src/main/java/sfCount/VisitCount/GVisitCount.java
+++ src/main/java/sfCount/VisitCount/GVisitCount.java
@@ -12,21 +12,22 @@ public class GVisitCount {
         String dbURL = "jdbc:sqlserver://10.10.10.101:1433; DatabaseName=safehelper"; //连接服务器和数据库sample
         String userName = "sa"; //默认用户名
         String userPwd = "123456"; //密码
-        Connection dbConn;
+
 
         try {
             Class.forName(driverName);
-            dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
-            //System.out.println( "Connection Successful! "); //如果连接成功 控制台输出Connection Successful!
+            try(Connection dbConn = DriverManager.getConnection(dbURL, userName, userPwd)){
+                //System.out.println( "Connection Successful! "); //如果连接成功 控制台输出Connection Successful!
 
-            Statement stmt = null;
-            String query = "select visit_count from sf_count where id=1";
+                String query = "select visit_count from sf_count where id=1";
 
-            stmt = dbConn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            rs.next();
+                try(Statement stmt = dbConn.createStatement()){
+                    ResultSet rs = stmt.executeQuery(query);
+                    rs.next();
 
-            visit_count = rs.getInt("visit_count");
+                    visit_count = rs.getInt("visit_count");
+                }
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
